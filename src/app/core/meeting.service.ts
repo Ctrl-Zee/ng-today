a9b6f8511ae1992c987c2ebd93d05b654d71999c
andrew.smith <andrew.smith@moserit.com>
2023-07-10 10:28:00 -0400

    create meetings for the current day

--- src/app/core/meeting.service.ts
+++ src/app/core/meeting.service.ts
@@ -2,36 +2,38 @@ import { Injectable } from '@angular/core';
 import { Meeting } from '../models/meeting';
 import { Observable, of } from 'rxjs';
 import { calculateHeightValue, calculateTopValue } from '../utilities/schedule-helper';
+import { getYear, getMonth, getDate } from 'date-fns';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MeetingService {
+  now = new Date();
   private meetingList: Meeting[] = [
     {
       title: 'Morning Stand-up',
-      start: new Date('2023-07-07T08:00:00'),
-      end: new Date('2023-07-07T08:15:00'),
+      start: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 8, 0, 0),
+      end: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 8, 15, 0),
     },
     {
       title: 'Project Review with Client',
-      start: new Date('2023-07-07T10:00:00'),
-      end: new Date('2023-07-07T11:00:00'),
+      start: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 10, 0, 0),
+      end: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 11, 0, 0),
     },
     {
       title: 'Design Team Sync',
-      start: new Date('2023-07-07T12:30:00'),
-      end: new Date('2023-07-07T13:30:00'),
+      start: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 12, 30, 0),
+      end: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 13, 30, 0),
     },
     {
       title: 'Budget Planning Session',
-      start: new Date('2023-07-07T14:00:00'),
-      end: new Date('2023-07-07T15:30:00'),
+      start: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 14, 0, 0),
+      end: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 15, 30, 0),
     },
     {
       title: 'Wrap-up and Next Steps Discussion',
-      start: new Date('2023-07-07T16:00:00'),
-      end: new Date('2023-07-07T17:00:00'),
+      start: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 16, 0, 0),
+      end: new Date(getYear(this.now), getMonth(this.now), getDate(this.now), 17, 0, 0),
     },
   ];
 
